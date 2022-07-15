4b44948f9b2bc3671a3f1c700bc0d730e3da52f2
alexg-lviv <alexg.lviv@gmail.com>
2022-07-15 23:41:22 +0300

    add process combat placeholder

--- game/src/Enemies/Enemy.gd
+++ game/src/Enemies/Enemy.gd
@@ -8,7 +8,6 @@ var effective_fighting_distance: int = 20
 var speed: int = 50
 var movable: bool = true
 
-var curr_direction: bool = true
 
 var distance: float
 var velocity: Vector2
@@ -27,17 +26,23 @@ func _process(delta):
 	distance = position.distance_to(Player.position)
 	direction_to_player = position.direction_to(Player.position)
 	velocity = direction_to_player * speed
+	
 	# stop when too close to player or if cant move (useful for freeze?)
 	if(movable && (distance > effective_fighting_distance)):
 		move_and_slide(velocity)
+	
 	# mirror enemy
 	if(direction_to_player.x < 0 && scale.y > 0): 
 		scale.x *= -1
 	elif(direction_to_player.x > 0 && scale.y < 0): 
 		scale.x *= -1
+	
+	# handle combat if possible
+	if(distance <= effective_fighting_distance):
+		handle_fight()
 
-
-
+func handle_fight():
+	pass
 
 func burn():
 	pass
