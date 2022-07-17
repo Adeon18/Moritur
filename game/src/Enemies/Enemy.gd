fd5e314a04af6b411b6626d290da8f52f140aef4
alexg-lviv <alexg.lviv@gmail.com>
2022-07-17 09:29:17 +0300

    add enemy freeze, add actual DOT and damage effects

--- game/src/Enemies/Enemy.gd
+++ game/src/Enemies/Enemy.gd
@@ -5,7 +5,7 @@ class_name Enemy
 #only for ranged units
 var projectile_speed = 100
 
-var health: int = 10
+var health: int = 100
 var effective_fighting_distance: int = 40
 export var is_hitting: bool = false
 
@@ -21,8 +21,10 @@ var burn_time: float = 3
 var freeze_time: float = 1
 var poison_time: float = 5
 
-var _bur_damage: int
+var _burn_damage: int
 var _poison_damage: int
+
+var is_frozen: bool = false
 
 var path = []
 
@@ -52,34 +54,37 @@ func _ready():
 
 
 func _process(delta):
-	# move to player
-	distance = position.distance_to(player.position)
-	direction_to_player = position.direction_to(player.position)
-	velocity = direction_to_player * speed
 	
-	raycast.set_cast_to(player.global_position - position)
-	
-	if(raycast.is_colliding()): can_hit = false
-	else: can_hit = true
+	# if enemy is frozen he cant do ANYTHING
+	if(!is_frozen):
+		# move to player
+		distance = position.distance_to(player.position)
+		direction_to_player = position.direction_to(player.position)
+		velocity = direction_to_player * speed
+		
+		raycast.set_cast_to(player.global_position - position)
+		
+		if(raycast.is_colliding()): can_hit = false
+		else: can_hit = true
 
-	
-	# stop when too close to player or if cant move (useful for freeze?)
-	if(movable && ((distance > effective_fighting_distance) || !can_hit)):
-		Statemachine.travel("run")
-		if(path.size() > 1):
-			var d = position.distance_to(path[0])
-			if (d > 1): position = position.linear_interpolate(path[0], speed*delta/d)
-			else:
-				path.remove(0)
-	
-	
-	# handle combat if possible
-	if((distance <= effective_fighting_distance) && can_hit):
-		handle_fight()
+		
+		# stop when too close to player or if cant move (useful for freeze?)
+		if(movable && ((distance > effective_fighting_distance) || !can_hit)):
+			Statemachine.travel("run")
+			if(path.size() > 1):
+				var d = position.distance_to(path[0])
+				if (d > 1): position = position.linear_interpolate(path[0], speed*delta/d)
+				else:
+					path.remove(0)
+		
+		
+		# handle combat if possible
+		if((distance <= effective_fighting_distance) && can_hit):
+			handle_fight()
 
 
-	# mirror enemy if needed
-	mirror()
+		# mirror enemy if needed
+		mirror()
 
 
 func update_path():
@@ -99,12 +104,16 @@ func mirror():
 		area2d.scale.x *= -1
 		weapon.scale.x *= -1
 
+
+
 func handle_fight():
 	pass
 
 func take_damage(damage):
-	print("hitt")
 	health -= damage
+	print(health)
+
+
 
 func _on_Area2D_area_entered(area):
 	if(area.is_in_group("Projectiles")):
@@ -113,12 +122,13 @@ func _on_Area2D_area_entered(area):
 
 func _on_BurnTimer_timeout():
 	print("OH NO ME BURN")
-	_bur_damage = 0
+	_burn_damage = 0
 	fire.visible = false
 
 func _on_FreezeTimer_timeout():
 	print("OH NO ME FREEZE")
 	ice.visible = false
+	is_frozen = false
 
 func _on_PoisonTimer_timeout():
 	print("OH NO ME POISONED")
@@ -131,18 +141,26 @@ func burn(damage):
 	print("Burn")
 	fire.visible = true
 	fire_timer.start(burn_time)
+	_burn_damage = damage/2
 
 func freeze(damage):
 	print("Freeze")
 	ice.visible = true
 	ice_timer.start(freeze_time)
+	is_frozen = true
 
 func poizon(damage):
 	print("poison")
 	poison.visible = true
 	poison_timer.start(poison_time)
+	_poison_damage = damage/4
 
 
 
 func _on_DOTticks_timeout():
-	take_damage(_poison_damage + _bur_damage)
+	if(_poison_damage + _burn_damage > 0):
+		take_damage(_poison_damage + _burn_damage)
+
+
+func _on_Timer_timeout():
+	update_path()
