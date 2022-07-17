3646bac7bc9472215cd0fa305e05f79c49e156b9
Bohdan <bodya.ru02@gmail.com>
2022-07-17 10:50:00 +0300

    Add forwards effect

--- game/src/Board/Board.gd
+++ game/src/Board/Board.gd
@@ -22,9 +22,9 @@ onready var cell = preload("res://src/Board/Cell/Cell.tscn")
 onready var Line = $Line2D
 onready var BoardPlayer = preload("res://src/Board/BoardPlayer/BoardPlayer.tscn")
 onready var Cam = $Camera2D
-onready var Die1 = $DieContainer/Die1
-onready var Die2 = $DieContainer/Die2
-onready var DieContainer = $DieContainer
+onready var Die1 = $CanvasLayer/DieContainer/Die1
+onready var Die2 = $CanvasLayer/DieContainer/Die2
+onready var DieContainer = $CanvasLayer/DieContainer
 
 var cell_types: Dictionary
 
@@ -142,7 +142,7 @@ func spawn_cells(path):
 func _process(delta):
 	if !player.can_roll or Input.is_action_just_pressed("focus_on_player"):
 		Cam.position = player.position
-	DieContainer.position = Cam.position
+#	DieContainer.position = Cam.position
 	
 	if Input.is_action_just_pressed("ui_accept") and player.can_roll:
 		var die_res = roll_die()
--- game/src/Board/Cell/RandomEffectCell.gd
+++ game/src/Board/Cell/RandomEffectCell.gd
@@ -3,33 +3,78 @@ extends "res://src/Board/Cell/Cell.gd"
 class_name RandomEffectCell
 
 enum EFFECTS {
-	GO_BACKWARDS
+	GO_BACKWARDS,
+	GO_FORWARDS
 }
+
+var effect_weight: Dictionary =  {
+	EFFECTS.GO_BACKWARDS: {
+		"roll_weight": 1
+	},
+	EFFECTS.GO_FORWARDS: {
+		"roll_weight": 1
+	},
+}
+
+const effects_functions: Dictionary = {
+	EFFECTS.GO_BACKWARDS: "go_backwards",
+	EFFECTS.GO_FORWARDS: "go_forwards",
+}
+
+onready var WaitOnStepTimer = $WaitOnStepTimer
+var curr_player
 
 var effect_name = EFFECTS.GO_BACKWARDS
 
-var backwards_step = 0
 
-const effects_functions: Dictionary = {
-	EFFECTS.GO_BACKWARDS: "go_backwards"
-}
+var backwards_step = 0
+var forwards_step = 0
 
 func initialize():
-	if effect_name == EFFECTS.GO_BACKWARDS:
-		backwards_step = randi() % 12 + 1
+	init_probabilities()
+	random_effect()
+	print(effects_functions[effect_name])
+	match effect_name:
+		EFFECTS.GO_BACKWARDS:
+			backwards_step = randi() % 12 + 1
+		EFFECTS.GO_FORWARDS:
+			forwards_step = randi() % 12 + 1
+
+var total_weight
+func init_probabilities():
+	total_weight = 0.0
+	for effect_type in effect_weight.values():
+		total_weight += effect_type.roll_weight
+		effect_type.acc_weight = total_weight
+		
+func random_effect():
+	var roll: float = rand_range(0.0, total_weight)
+	for effect_type in effect_weight.keys():
+		if effect_weight[effect_type].acc_weight > roll:
+			effect_name = effect_type
+			return
 
 func on_step(player):
+	curr_player = player
+	player.can_roll = false
 	print("you stepped on mysterious cell")
 	if was_stepped_on:
+		player.can_roll = true
 		print("you already stepped on this cell")
 		return
 
-	was_stepped_on = true
-	after_step()
-	call_deferred(effects_functions[effect_name], player)
-
+	WaitOnStepTimer.start()
 
 
 func go_backwards(player):
+	print("player going backwards for ", backwards_step, " cells")
 	player.go_backwards(backwards_step)
 
+func go_forwards(player):
+	print("player going forwards for ", forwards_step, " cells")
+	player.move_player(forwards_step)
+
+func _on_WaitOnStepTimer_timeout():
+	was_stepped_on = true
+	after_step()
+	call_deferred(effects_functions[effect_name], curr_player)
